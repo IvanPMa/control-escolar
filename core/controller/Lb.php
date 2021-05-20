daf5f4c5227507835cb959202e0d219c37f3241c
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-05-19 23:14:03 -0500

    Control Escolar V1

---
+++ core/controller/Lb.php
@@ -0,0 +1,29 @@
+<?php
+
+
+// 10 de Febrero  del 2021
+// Lb.php
+
+class Lb {
+
+	public function __construct(){
+	}
+
+	public function start(){
+		if(isset($_GET)){
+			foreach($_GET as $k=>$v){
+				Core::$get[$k]=$v;
+			}
+		}
+		if(isset($_POST)){
+			foreach($_POST as $k=>$v){
+				Core::$post[$k]=$v;
+			}
+		}
+		include "core/app/autoload.php";
+		include "core/app/init.php";
+	}
+
+}
+
+?>
---
+++ core/controller/Module.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// 19 de Febrero del 2021
+// Module.php
+// @brief tareas que se realizan con modulos.
+
+class Module {
+
+	public static function loadLayout(){
+		include "core/app/layouts/layouta.php";
+	}
+
+
+}
+
+
+
+?>
